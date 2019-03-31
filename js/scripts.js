8e28ff378d4fe616a93198870a434aa1d4d5eb58
abigailwambui <abigailw15njuguna@gmail.com>
2019-03-31 23:14:55 +0300

    Add message arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,38 @@
-$(document).ready(function() {
-  $("#start").click(function() {
-    $("#drop").slideToggle();
-  });
-});
+function check(){
+  var question1= document.(".quiz").("#question1").value;
+  var question2= document.(".quiz").("#question2").value;
+  var question3= document.(".quiz").("#question3").value;
+  var question4= document.(".quiz").("#question4").value;
+  var question5= document.(".quiz").("#question5").value;
+  var question6= document.(".quiz").("#question6").value;
+  var question7= document.(".quiz").("#question7").value;
+  var question8= document.(".quiz").("#question8").value;
+  var correct = 0;
+
+      if ("#question1" == "Javascript") {
+        correct +=10;
+      }
+      if ("#question2" == ".toLowerCase()") {
+        correct +=10;
+      }
+      if ("#question3" == "Cosnsole") {
+        correct +=10;
+      }
+      if ("#question4" == "Enables a more interactive session between the user and the website") {
+        correct +=10;
+      }
+      if ("#question5" == "SCRIPT") {
+        correct +=10;
+      }
+      if ("#question6" == "document.write("Hello world")") {
+        correct +=10;
+      }
+      if ("#question7" == "if") {
+        correct +=10;
+      }
+      if ("#question8" == ".splice()") {
+        correct +=10;
+      }
+  document.getElementById("after-submit").style.visibility = "visible";
+  document.getElementById("number-correct").innerHTML= "You got "+ correct+ "correct";
+}
